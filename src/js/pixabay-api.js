68fb62275a5df565625674b0de9ba7c9d754be0a
Denis Shvyrkov <denysbc2011@gmail.com>
2024-02-24 22:59:51 -0800

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,12 @@
-'use strict';
 import axios from 'axios';
 
 export async function searchImages(searchTerm, page, perPage) {
   axios.defaults.baseURL = 'https://pixabay.com';
-  const END_POINT = '/api';
-  const resp = await axios.get(END_POINT, {
-    params: {
-      key: '21768935-3fedd5c602a3f7ac5e18d4c15',
-      q: `${searchTerm}`,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: `${page}`,
-      per_page: `${perPage}`,
-    },
-  });
+  const END_POINT = '/api/';
+  const KEY = '42535847-effe6d5cdde3e67806355c12e&q';
+  const IMG_TYPE = 'photo';
+  const ORIENTATION = 'horizontal';
+  const PARAMS = `?key=${KEY}&q=${searchTerm}&image_type=${IMG_TYPE}&orientation=${ORIENTATION}&safesearch=true&page=${page}&per_page=${perPage}`;
+  const resp = await axios.get(END_POINT + PARAMS);
   return resp;
 }
